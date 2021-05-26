c101e1dba3197c310ee34c9447f2db2ab4a53692
Soulaiman <[soulaimanmeskini@gmail.com]>
2021-05-26 15:56:44 +0200

    loading screen + look book begin

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-// cursor // 
+// loader // 
+var loader
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.03)
+        }, 100);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementsById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1)
+});
+
+/* cursor*/
 
 const cursor = document.querySelector('.pointer');
 document.addEventListener('mousemove', e => {
